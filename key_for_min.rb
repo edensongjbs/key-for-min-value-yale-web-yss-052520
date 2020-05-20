3d1e6f165e29c54f8f9b6b562d36c83463205f88
James B Schoen <edensongjbs@gmail.com>
2020-05-20 12:38:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   min_key="nothing"
-  name_hash.reduce(0) do |memo, (key, value)|
+  name_hash.reduce(nil) do |memo, (key, value)|
     puts "Memo: #{memo} Key: #{key} Value: #{value}"
-    if value<memo
+    if !memo || value<memo
       memo=value
       min_key=key
     end
