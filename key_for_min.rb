d0bc82d7ce34045f8f7c13417aa12094d9711f92
James B Schoen <edensongjbs@gmail.com>
2020-05-20 12:42:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_key="nothing"
+  #min_key=nil
   name_hash.reduce(nil) do |memo, (key, value)|
     puts "Memo: #{memo} Key: #{key} Value: #{value}"
     if !memo || value<memo
